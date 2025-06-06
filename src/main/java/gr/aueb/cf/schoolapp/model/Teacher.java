41775a447bf544f44fadb38e67b85890c3f7949c
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-06 17:49:01 +0300

    feat: teacher entity added

--- src/main/java/gr/aueb/cf/schoolapp/model/Teacher.java
+++ src/main/java/gr/aueb/cf/schoolapp/model/Teacher.java
@@ -1,5 +1,25 @@
 package gr.aueb.cf.schoolapp.model;
 
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "teachers")
 public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private Long uuid;
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+
 }
