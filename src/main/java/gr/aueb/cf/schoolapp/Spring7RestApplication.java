0c0adfd7b56e40653c0ef1aaad8a176d2bae842b
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-06 17:23:02 +0300

    first commit

---
+++ src/main/java/gr/aueb/cf/schoolapp/Spring7RestApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.schoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring7RestApplication.class, args);
+	}
+
+}
