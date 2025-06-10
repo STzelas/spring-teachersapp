21e69d2355d268a7d56f20b05bd8dca26256aefc
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-10 21:06:29 +0300

    chore: comments

--- src/main/java/gr/aueb/cf/schoolapp/repository/EmployeeRepository.java
+++ src/main/java/gr/aueb/cf/schoolapp/repository/EmployeeRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@Repository // Δεν είναι απαραίτητο
 public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
 }
