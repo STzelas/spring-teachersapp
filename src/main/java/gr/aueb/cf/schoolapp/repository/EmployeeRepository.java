5796585f726278ef9f21e0f4c20a3c5b7eb0d132
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-10 21:05:29 +0300

    feat: employee repository added

---
+++ src/main/java/gr/aueb/cf/schoolapp/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.schoolapp.repository;
+
+import gr.aueb.cf.schoolapp.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
+}
