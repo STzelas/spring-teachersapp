723d73b3479fccbf4ed5561083abd567e046f7ab
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-16 21:19:06 +0300

    refactor: vat -> amka

---
+++ src/main/java/gr/aueb/cf/schoolapp/core/filters/TeacherFilters.java
@@ -0,0 +1,25 @@
+package gr.aueb.cf.schoolapp.core.filters;
+
+import lombok.*;
+import org.springframework.lang.Nullable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class TeacherFilters {
+
+    @Nullable
+    private String uuid;
+
+    @Nullable
+    private String userAfm;
+
+    @Nullable
+    private String userAmka;
+
+    @Nullable
+    private Boolean isActive;
+
+
+}
