76de3cc70ed24b389f1b4209359c031a7cbcccb8
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-17 20:04:30 +0300

    feat: amka is unique now

--- src/main/java/gr/aueb/cf/schoolapp/model/PersonalInfo.java
+++ src/main/java/gr/aueb/cf/schoolapp/model/PersonalInfo.java
@@ -18,6 +18,7 @@ public class PersonalInfo extends AbstractEntity{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String amka;
 
     @Column(name = "identity_number")
