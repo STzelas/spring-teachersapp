7acf93b07e35bf113e675de7bbb877f3b0744ebc
Sot. Tzelatidis <stzelati@gmail.com>
2025-06-17 20:10:03 +0300

    chore: todo added

--- src/main/java/gr/aueb/cf/schoolapp/SchoolAppApplication.java
+++ src/main/java/gr/aueb/cf/schoolapp/SchoolAppApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+/**
+ * todo:
+ * 	1. fix swagger, fix bcrypt - Empty encoded password
+ */
 @SpringBootApplication
 @EnableJpaAuditing // created_at, updated_at
 public class SchoolAppApplication {
